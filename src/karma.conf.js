45e3ace4335eb07ec6b8d91d170e0349482f7499
Stephen Fluin <stephen.fluin@gmail.com>
2018-07-04 02:11:45 -0700

    add lint and unit tests to travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,6 +3,13 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: [ 'MyHeadlessChrome'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -25,7 +32,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false
   });
 };
